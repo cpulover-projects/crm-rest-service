16bea9e3878632f117c3e806769e08c02519e389
cpulover <justacpulover@gmail.com>
2020-07-10 20:59:02 +0200

    Basic configuration

---
+++ src/main/java/com/cpulover/springdemo/dao/CustomerDAO.java
@@ -0,0 +1,17 @@
+package com.cpulover.springdemo.dao;
+
+import java.util.List;
+
+import com.cpulover.springdemo.entity.Customer;
+
+public interface CustomerDAO {
+
+	public List<Customer> getCustomers();
+
+	public void saveCustomer(Customer theCustomer);
+
+	public Customer getCustomer(int theId);
+
+	public void deleteCustomer(int theId);
+	
+}
